0dad8afa54df4cdee3fbfb183ab66486b7674f17
Manoj Nama <manoj.nama@outlook.com>
2016-02-21 17:10:13 +0530

    Added grunt tasks to compile and watch scss files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,30 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function (grunt) {
+
+   grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      sass: {
+         dev: {
+            options: {
+               style: "expanded"
+            },
+            files: {
+               "public/assets/styles/main.css": "public/assets/styles/main.scss"
+            }
+         }
+      },
+      watch: {
+         compass: {
+            files: ["public/**/*.{scss,sass}"],
+            tasks: ["sass:dev"]
+         }
+      }
+   });
+
+   grunt.loadNpmTasks("grunt-contrib-sass");
+   grunt.loadNpmTasks("grunt-contrib-watch");
+
+   grunt.registerTask("default", ["sass", "watch"]);
 
 };
